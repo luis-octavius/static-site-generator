5863423b6f98bf7aa7842ad56c8e7e95328762e1
luis-octavius <luisoctavius.sc@gmail.com>
2025-09-09 19:25:58 -0300

    fix: eq method, constructor and enum

--- src/textnode.py
+++ src/textnode.py
@@ -1,22 +1,23 @@
 from enum import Enum 
 
-class Bender(Enum):
-    AIR_BENDER = "air"
-    WATER_BENDER = "water"
-    EARTH_BENDER = "earth"
-    FIRE_BENDER = "fire"
+class TextType(Enum): 
+    PLAIN = "text" 
+    BOLD = "bold"
+    ITALIC = "italic"
+    CODE = "code"
+    LINK = "link"
+    IMAGE = "image"
 
 
 class TextNode():
-    def __init__(self, text, text_type, url):
+    def __init__(self, text, text_type, url = None):
         self.text = text 
         self.text_type = text_type
         self.url = url 
 
     def __eq__(self, other):
-        if isistance(other, TextNode):
-            return self.text_type == other.text_type
-        
+        if isinstance(other, TextNode):
+            return self.text_type == other.text_type and self.url == other.url and self.text == other.text 
 
     def __repr__(self):
         return f"TextNode({self.text}, {self.text_type}, {self.url})"
