772e15bd21706e0a2f4ccd156489e3383d54de65
luis-octavius <luisoctavius.sc@gmail.com>
2025-09-10 15:24:03 -0300

    test: tests for parent nodes

--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from htmlnode import HTMLNode, LeafNode 
+from htmlnode import HTMLNode, LeafNode, ParentNode
 
 class TestHTMLNode(unittest.TestCase):
     def test_eq(self):
@@ -15,7 +15,6 @@ class TestHTMLNode(unittest.TestCase):
         }
         node = HTMLNode("a", props=dict_props)
         str_props = node.props_to_html()
-        print("Str props: ", str_props)
         str_output_test = ' href="https://www.boot.dev" target="_blank"'
         self.assertEqual(str_props, str_output_test)
 
@@ -43,3 +42,30 @@ class TestLeafNode(unittest.TestCase):
     def test_leaf_to_html_h1(self):
         node = LeafNode("h1", "Hey Jude")
         self.assertEqual(node.to_html(), '<h1>Hey Jude</h1>')
+
+class TestParentNode(unittest.TestCase):
+    def test_to_html_with_children(self):
+        child_node = LeafNode("span", "child")
+        parent_node = ParentNode("div", [child_node])
+        self.assertEqual(parent_node.to_html(), "<div><span>child</span></div>")
+
+    def test_to_html_with_grandchildren(self):
+        grandchild_node = LeafNode("b", "grandchild")
+        child_node = ParentNode("span", [grandchild_node])
+        parent_node = ParentNode("div", [child_node])
+        self.assertEqual(
+            parent_node.to_html(),
+            "<div><span><b>grandchild</b></span></div>",
+        )
+
+    def test_to_html_many_children_with_props(self):
+        node = ParentNode("p", [
+            LeafNode("a", "Go to Google", {"href": "https://www.google.com"}),
+            LeafNode("p", "Bold Text", {"class": "bold-mode"}),
+            LeafNode("span", "Relax", {"class": "span-mode"}),
+            ],
+        )
+        self.assertEqual(
+            node.to_html(),
+            '<p><a href="https://www.google.com">Go to Google</a><p class="bold-mode">Bold Text</p><span class="span-mode">Relax</span></p>'
+        )
