6ae987a3a341034d8c105ad520c53b104ea48949
luis-octavius <luisoctavius.sc@gmail.com>
2025-09-09 21:23:36 -0300

    feat: class to handle leaf nodes

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -4,7 +4,7 @@ class HTMLNode():
     def __init__(self, tag = None, value = None, children = None, props = None):
         self.tag = tag
         self.value = value 
-        self.children = value 
+        self.children = children  
         self.props = props 
 
     def to_html(self):
@@ -18,6 +18,7 @@ class HTMLNode():
 
             print("String: ", str)
             return str 
+        return 
 
     def __repr__(self):
         return f"""
@@ -31,5 +32,20 @@ props: {self.props}
         if isinstance(other, HTMLNode):
             return self.tag == other.tag and self.value == other.value and self.children == other.children and self.props == other.props 
 
+class LeafNode(HTMLNode):
+    def __init__(self, tag, value, props = None):
+        super().__init__(tag, value, None, props)
 
+    def to_html(self):
+        if not self.value:
+            raise ValueError 
+        
+        if not self.tag:
+            return str(self.value)
 
+        print("Props: ", self.props)
+        
+        if self.props == None:
+            return f'<{self.tag}>{self.value}</{self.tag}>'
+        else:
+            return f'<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>'
