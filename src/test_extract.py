b2c14e7c4c15c4850a1b2c0225d25e3aa788b879
luis-octavius <luisoctavius.sc@gmail.com>
2025-09-11 19:17:34 -0300

    test: extract

---
+++ src/test_extract.py
@@ -0,0 +1,19 @@
+import unittest 
+from extract import extract_markdown_images
+
+class TextExtract(unittest.TestCase):
+    def test_eq_image(self):
+        text = "This is text with a ![rick roll](https://i.imgur.com/aKaOqIh.gif) and ![obi wan](https://i.imgur.com/fJRm4Vk.jpeg)"
+        self.assertEqual(
+            extract_markdown_images(text), 
+            [("rick roll", "https://i.imgur.com/aKaOqIh.gif"), ("obi wan", "https://i.imgur.com/fJRm4Vk.jpeg")]
+                         )
+
+    def test_eq_link(self):
+        text = "This is text with a link [to boot dev](https://www.boot.dev) and [to youtube](https://www.youtube.com/@bootdotdev)"
+
+        self.assertEqual(
+            extract_markdown_images(text),
+            [("to boot dev", "https://www.boot.dev"), ("to youtube", "https://www.youtube.com/@bootdotdev")]
+        )
+
