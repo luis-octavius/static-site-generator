9d6069b6ddd55420b7853b1f6d3d260034afe6d8
luis-octavius <luisoctavius.sc@gmail.com>
2025-09-15 11:13:27 -0300

    refactor: remove markdown_to_html_node import

--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from htmlnode import HTMLNode, LeafNode, ParentNode, markdown_to_html_node
+from htmlnode import HTMLNode, LeafNode, ParentNode
 
 class TestHTMLNode(unittest.TestCase):
     def test_eq(self):
@@ -32,7 +32,6 @@ class TestHTMLNode(unittest.TestCase):
 class TestLeafNode(unittest.TestCase):
     def test_leaf_to_html_p(self):
         node = LeafNode("p", "Hello, world!")
-        print("Node: ", node)
         self.assertEqual(node.to_html(), "<p>Hello, world!</p>")
 
     def test_leaf_to_html_a(self):
@@ -68,35 +67,4 @@ class TestParentNode(unittest.TestCase):
         self.assertEqual(
             node.to_html(),
             '<p><a href="https://www.google.com">Go to Google</a><p class="bold-mode">Bold Text</p><span class="span-mode">Relax</span></p>'
-        )
-    def test_paragraphs(self):
-        md = """
-This is **bolded** paragraph
-text in a p
-tag here
-
-This is another paragraph with _italic_ text and `code` here
-
-"""
-
-        node = markdown_to_html_node(md)
-        html = node.to_html()
-        self.assertEqual(
-        html,
-        "<div><p>This is <b>bolded</b> paragraph text in a p tag here</p><p>This is another paragraph with <i>italic</i> text and <code>code</code> here</p></div>",
-    )
-
-    def test_codeblock(self):
-        md = """
-    ```
-    This is text that _should_ remain
-    the **same** even with inline stuff
-    ```
-    """
-
-        node = markdown_to_html_node(md)
-        html = node.to_html()
-        self.assertEqual(
-            html,
-            "<div><pre><code>This is text that _should_ remain\nthe **same** even with inline stuff\n</code></pre></div>",
         )
