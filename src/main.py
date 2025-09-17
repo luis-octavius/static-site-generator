aaa462a0654e7fb68855e314a418b20bdcce8b37
luis-octavius <luisoctavius.sc@gmail.com>
2025-09-16 22:06:48 -0300

    refactor: change variable dst_path to dest_path for clarity

--- src/main.py
+++ src/main.py
@@ -1,41 +1,37 @@
 import os 
 import shutil
 from textnode import TextNode
+from generate_page import generate_page
 
-def copy_dir_contents(src_dir, dst_dir):
+def copy_dir_contents(src_dir, dest_dir):
     abs_src = os.path.abspath(src_dir) # 
-    dst_src = os.path.abspath(dst_dir)
+    dest_src = os.path.abspath(dest_dir)
     list_dir = os.listdir(abs_src)
-    print("list dir: ", list_dir)
 
     for file in list_dir:
         src_path = os.path.join(abs_src, file)
-        print("Src path file: ", src_path)
-        dst_path = os.path.join(dst_src, file) 
-        print("Dst path file: ", dst_path)
+        dest_path = os.path.join(dest_src, file) 
 
         if os.path.isfile(src_path):
-            os.makedirs(os.path.dirname(dst_path), exist_ok = True)
-            shutil.copy(src_path, dst_path)
-            print("Arquivo copiado para: ", dst_path)
+            os.makedirs(os.path.dirname(dest_path), exist_ok = True)
+            shutil.copy(src_path, dest_path)
+            print("Arquivo copiado para: ", dest_path)
 
         if os.path.isdir(src_path):
-            os.makedirs(dst_path, exist_ok = True)
-            copy_dir_contents(src_path, dst_path)
+            os.makedirs(dest_path, exist_ok = True)
+            copy_dir_contents(src_path, dest_path)
 
 def check_public():
     public_path = os.path.abspath(os.path.join("./", "public")) 
-    print("public path: ", public_path)
     if os.path.exists(public_path):
         print("EXISTS")
         shutil.rmtree(public_path)
         os.mkdir(public_path)
-        
-
 
 def main():
     check_public()
     copy_dir_contents("static", "public")
+    generate_page("content/index.md", "template.html", "public/index.html")
 
 
 main()
